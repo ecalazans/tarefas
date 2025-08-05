446ead9946d8dee877081947bff466447f11ef48
ecalazans <erickdcalazans@gmail.com>
2025-08-05 00:54:10 -0300

    chore: connect database and functions tasks page

--- src/pages/dashboard/index.tsx
+++ src/pages/dashboard/index.tsx
@@ -1,4 +1,4 @@
-import { ChangeEvent, FormEvent, useState } from "react";
+import { ChangeEvent, FormEvent, useEffect, useState } from "react";
 import { GetServerSideProps } from "next";
 import { getSession } from "next-auth/react";
 
@@ -8,23 +8,103 @@ import Head from "next/head";
 import { Textarea } from "../../components/textarea"
 import { FiShare2 } from "react-icons/fi";
 import { FaTrash } from "react-icons/fa";
+import Link from "next/link";
 
-export default function Dashboard() {
+import { db } from "../../services/firebaseConnection"
+import { addDoc, collection, query, orderBy, onSnapshot, where, doc, deleteDoc } from "firebase/firestore";
+
+interface DashProps {
+  user: {
+    email: string
+  }
+}
+
+interface TaskPropos {
+  id: string;
+  created: Date;
+  public: boolean;
+  tarefa: string;
+  user: string;
+}
+
+export default function Dashboard({ user }: DashProps) {
+
   const [input, setInput] = useState("")
   const [publicTask, setPublicTask] = useState(false)
+  const [tasks, setTasks] = useState<TaskPropos[]>([])
 
   function handleChangePublic(event: ChangeEvent<HTMLInputElement>) {
-    console.log(event.target.checked)
     setPublicTask(event.target.checked)
   }
 
-  function handleRegisterTask(event: FormEvent) {
+  async function handleShare(id: string) {
+    await navigator.clipboard.writeText(
+      `${process.env.NEXT_PUBLIC_URL}/task/${id}`
+    )
+    alert('URL copiada com sucesso!')
+  }
+
+  async function handleDeleteTask(id: string) {
+    const docRef = doc(db, "tarefas", id)
+
+    await deleteDoc(docRef)
+  }
+
+  async function handleRegisterTask(event: FormEvent) {
     event.preventDefault()
 
     if (input === "") return
 
-    alert("TESTE")
+    try {
+      await addDoc(collection(db, "tarefas"), {
+        tarefa: input,
+        created: new Date(),
+        user: user?.email,
+        public: publicTask
+      })
+
+      alert("Tarefa cadastrada com sucesso!!!")
+
+      setInput("")
+      setPublicTask(false)
+
+    } catch (error) {
+      console.log(error)
+    }
+
   }
+
+  useEffect(() => {
+    async function loadTatefas() {
+      const tarefasRef = collection(db, "tarefas")
+      const q = query(
+        tarefasRef,
+        orderBy("created", "desc"),
+        where("user", "==", user?.email)
+      )
+
+      //onSnapshot
+      // sempre fica verificando/observando o banco para caso de laterações
+      onSnapshot(q, (snapshot) => {
+        let lista = [] as TaskPropos[]
+
+        snapshot.forEach((doc) => {
+          lista.push({
+            id: doc.id,
+            created: doc.data().created,
+            public: doc.data().public,
+            tarefa: doc.data().tarefa,
+            user: doc.data().user,
+          })
+        })
+
+        setTasks(lista)
+      })
+
+    }
+
+    loadTatefas()
+  }, [user?.email])
 
   return (
     <div className={styles.container}>
@@ -64,26 +144,39 @@ export default function Dashboard() {
         <section className={styles.taskContainer}>
           <h1>Minhas tarefas</h1>
 
-          <article className={styles.task}>
-            <div className={styles.tagContainer}>
-              <label className={styles.tag}>PUBLICO</label>
-              <button className={styles.shareButton}>
-                <FiShare2
-                  size={32}
-                  color="#3183ff"
-                />
-              </button>
-            </div>
-            <div className={styles.taskContent}>
-              <p>Minha primeira tarefa de exemplo</p>
-              <button className={styles.trashButton}>
-                <FaTrash
-                  size={24}
-                  color="#ea3140"
-                />
-              </button>
-            </div>
-          </article>
+          {tasks.map((item) => (
+            <article key={item.id} className={styles.task}>
+              {item.public && (
+                <div className={styles.tagContainer}>
+                  <label className={styles.tag}>PUBLICO</label>
+                  <button className={styles.shareButton}
+                    onClick={() => handleShare(item.id)}
+                  >
+                    <FiShare2
+                      size={32}
+                      color="#3183ff"
+                    />
+                  </button>
+                </div>
+              )}
+              <div className={styles.taskContent}>
+                {item.public ? (
+                  <Link href={`/task/${item.id}`}>
+                    <p>{item.tarefa}</p>
+                  </Link>
+                ) : (
+                  <p>{item.tarefa}</p>
+                )}
+                <button className={styles.trashButton}>
+                  <FaTrash
+                    size={24}
+                    color="#ea3140"
+                    onClick={() => handleDeleteTask(item.id)}
+                  />
+                </button>
+              </div>
+            </article>
+          ))}
         </section>
       </main>
     </div >
@@ -105,6 +198,10 @@ export const getServerSideProps: GetServerSideProps = async ({ req }) => {
   }
 
   return {
-    props: {},
+    props: {
+      user: {
+        email: session?.user?.email
+      }
+    },
   }
 }
